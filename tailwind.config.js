849c3e4814cf3a373f7a57d104007d967da92d4d
JoseInostroza <87823768+JoseInostroza@users.noreply.github.com>
2023-02-20 11:49:48 -0300

    primer commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundColor:{
+        complementary : '#FF7777'
+      },
+      backgroundImage: {
+        'nahala' : "URL(/src/img/nahala.jpg)",
+        'kroz' : "URL(/src/img/kroz.jpg)",
+        'dove' : "URL(/src/img/dove.jpg)",
+        'trini' : "URL(/src/img/trini.jpg)",
+        'gasef' : "URL(/src/img/gasef.png)",
+        'souken' : "URL(/src/img/souken.png)",
+        'cuero' : "URL(https://www.eliteguias.com/img/juegos/dark-souls-iii/armadura-de-cuero-negra.jpg)",
+        'cuerot' : "URL(https://www.eliteguias.com/img/juegos/dark-souls-iii/armadura-de-cuero-rigido.jpg)",
+        'pieles' : "URL(https://www.eliteguias.com/img/juegos/dark-souls-iii/armadura-de-caballero-caido.jpg)",
+        'mallaC' : "URL(https://www.eliteguias.com/img/juegos/dark-souls-iii/armadura-de-desertor.jpg)",
+        'escamas' : "URL(https://i.pinimg.com/736x/f7/3f/1c/f73f1cf1542e229d9f57ecdca3b688be.jpg)"
+      },
+      fontFamily: {
+        EB : ['EB Garamond', 'serif']
+      }
+    },
+  },
+  plugins: [],
+}
