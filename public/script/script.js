cf2943d67434f48b3b2b987706ac3812ce7e969e
JoseInostroza <87823768+JoseInostroza@users.noreply.github.com>
2023-03-02 12:47:32 -0300

    agrgando productos fase 1

--- public/script/script.js
+++ public/script/script.js
@@ -1,6 +1,13 @@
 var billeteras = document.querySelectorAll('#personajes span')
-
+var armadura_ligera = document.querySelectorAll('#liviana p')
+const botonLigera = document.getElementsByName('aLigera')
+const lienzo = document.querySelector('#carrito')
+let n
+let p
+let total = {'PP':0,'PO':0,'PT':0,'PC':0};
 let listaJugadores = {'nahala':{'PP':0,'PO':15,'PT':0,'PC':0},'kroz':{'PP':0,'PO':2,'PT':0,'PC':0},'dove':{'PP':0,'PO':7,'PT':0,'PC':0},'layka':{'PP':0,'PO':25,'PT':0,'PC':0},'gasef':{'PP':0,'PO':7,'PT':0,'PC':0},'sou-ken':{'PP':0,'PO':10,'PT':0,'PC':0},}
+
+
 
 billeteras.forEach((b)=>{
     switch(b.id){
@@ -30,4 +37,18 @@ billeteras.forEach((b)=>{
             break
     }
 })
-console.log(billeteras)
+
+botonLigera.forEach((e)=>{
+    e.addEventListener('click',()=>{
+        let activo = e.id
+        n = document.querySelector(`p[name=${activo}Titulo]`)
+        p = document.querySelector(`p[name=${activo}Precio]`)
+        let nombre = n.innerHTML
+        let precio = p.innerHTML
+        lienzo.innerHTML += `<div class=' '><p> ${nombre}<p> <p>${precio}<p>`   
+        var datos = precio.split(':')
+        total[datos[0]] += parseInt(datos[1])
+        var subtotal = document.getElementById(datos[0])
+        subtotal.innerHTML = total[datos[0]]
+    })
+})
