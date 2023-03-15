1fccfe3700593cae2602b1c62649486376185510
JoseInostroza <87823768+JoseInostroza@users.noreply.github.com>
2023-03-15 19:38:24 -0300

    terminando la logica de compras v.1

--- public/script/script.js
+++ public/script/script.js
@@ -5,59 +5,60 @@ const lienzo = document.querySelector('#carrito')
 const jugadores = document.querySelectorAll('#personajes input')
 const botonCompras = document.getElementById('botonCompra')
 const comprador = document.getElementById('comprador')
+var aviso = document.getElementById('aviso')
 let n
 let p
 let base = 0
 let billeteraActiva
-let creditoDisponible
+var creditoDisponible = 0
 let total = {'PP':0,'PO':0,'PT':0,'PC':0};
 const listaJugadores = {'nhala':{'PP':0,'PO':15,'PT':0,'PC':0},'kroz':{'PP':0,'PO':2,'PT':0,'PC':0},'dove':{'PP':0,'PO':7,'PT':0,'PC':0},'layka':{'PP':0,'PO':25,'PT':0,'PC':0},'gasef':{'PP':0,'PO':7,'PT':0,'PC':0},'sou-ken':{'PP':0,'PO':10,'PT':0,'PC':0},}
+let jugadorActivo
 
 function baseC(m,c){
     switch(m){
         case 'PP':
-            base += parseInt(c)*1000
-            break
+            return parseInt(c)*1000
         case 'PO':
-            base += parseInt(c)*100
-            break
+            return parseInt(c)*100
         case 'PT':
-            base += parseInt(c)*10
-            break
+            return parseInt(c)*10
         case 'PC':
-            base += parseInt(c)
-            break
+            return parseInt(c)
     }
 }
 
-billeteras.forEach((b)=>{
-    switch(b.id){
-        case 'billetera-nhala':
-            var a = listaJugadores['nhala']
-            b.innerHTML=`PP:${a['PP']} PO:${a['PO']} PT:${a['PT']} PC:${a['PC']}`;
-            break
-        case 'billetera-kroz':
-            var a = listaJugadores['kroz']
-            b.innerHTML=`PP:${a['PP']} PO:${a['PO']} PT:${a['PT']} PC:${a['PC']}`;
-            break
-        case 'billetera-layka':
-            var a = listaJugadores['layka']
-            b.innerHTML=`PP:${a['PP']} PO:${a['PO']} PT:${a['PT']} PC:${a['PC']}`;
-            break
-        case 'billetera-dove':
-            var a = listaJugadores['dove']
-            b.innerHTML=`PP:${a['PP']} PO:${a['PO']} PT:${a['PT']} PC:${a['PC']}`;
-            break
-        case 'billetera-gasef':
-            var a = listaJugadores['gasef']
-            b.innerHTML=`PP:${a['PP']} PO:${a['PO']} PT:${a['PT']} PC:${a['PC']}`;
-            break
-        case 'billetera-souken':
-            var a = listaJugadores['sou-ken']
-            b.innerHTML=`PP:${a['PP']} PO:${a['PO']} PT:${a['PT']} PC:${a['PC']}`;
-            break
-    }
-})
+function actualizar(){
+        billeteras.forEach((b)=>{
+        switch(b.id){
+            case 'billetera-nhala':
+                var a = listaJugadores['nhala']
+                b.innerHTML=`PP:${a['PP']} PO:${a['PO']} PT:${a['PT']} PC:${a['PC']}`;
+                break
+            case 'billetera-kroz':
+                var a = listaJugadores['kroz']
+                b.innerHTML=`PP:${a['PP']} PO:${a['PO']} PT:${a['PT']} PC:${a['PC']}`;
+                break
+            case 'billetera-layka':
+                var a = listaJugadores['layka']
+                b.innerHTML=`PP:${a['PP']} PO:${a['PO']} PT:${a['PT']} PC:${a['PC']}`;
+                break
+            case 'billetera-dove':
+                var a = listaJugadores['dove']
+                b.innerHTML=`PP:${a['PP']} PO:${a['PO']} PT:${a['PT']} PC:${a['PC']}`;
+                break
+            case 'billetera-gasef':
+                var a = listaJugadores['gasef']
+                b.innerHTML=`PP:${a['PP']} PO:${a['PO']} PT:${a['PT']} PC:${a['PC']}`;
+                break
+            case 'billetera-souken':
+                var a = listaJugadores['sou-ken']
+                b.innerHTML=`PP:${a['PP']} PO:${a['PO']} PT:${a['PT']} PC:${a['PC']}`;
+                break
+        }
+})}
+
+document.addEventListener('DOMContentLoaded',actualizar())
 botonArmadura.forEach((e)=>{
     e.addEventListener('click',()=>{
         let activo = e.id
@@ -67,7 +68,7 @@ botonArmadura.forEach((e)=>{
         let precio = p.innerHTML
         lienzo.innerHTML += `<div class='text-xs shadow-lg rounded-t-md mb-2'><span>${nombre}</span> <span>${precio}</span>`   
         var datos = precio.split(':')
-        baseC(datos[0],datos[1])
+        base += baseC(datos[0],datos[1])
         total[datos[0]] += parseInt(datos[1])
         var subtotal = document.getElementById(datos[0])
         subtotal.innerHTML = total[datos[0]]
@@ -77,9 +78,44 @@ botonArmadura.forEach((e)=>{
 jugadores.forEach((i)=>{
     i.addEventListener('change',(radio)=>{
         if (i.checked){
-            billeteraActiva = listaJugadores[i.id]
-            comprador.innerText = `${i.id}`
-            console.log(comprador)
+            jugadorActivo = i.id
+            billeteraActiva = listaJugadores[jugadorActivo]
+            creditoDisponible = 0
+            comprador.innerText = `${jugadorActivo}`
+            for (var moneda in billeteraActiva){
+                creditoDisponible += baseC(moneda,billeteraActiva[moneda])
+            }
         }
     })
+})
+
+botonCompras.addEventListener('click', ()=>{
+    if (creditoDisponible < base ){
+        aviso.innerHTML='Fondos insuficientes amigo, pide ayuda a tus compañeros'
+    }
+    else{
+        resto = creditoDisponible - base
+        j= listaJugadores[jugadorActivo]
+        for (moneda in j){
+            switch(moneda){
+                case 'PP':
+                    j['PP']=resto/1000
+                    resto = resto%1000
+                    break
+                case 'PO':
+                    j['PO']=resto/100
+                    resto = resto%100
+                    break
+                case 'PT':
+                    j['PT']=resto/10
+                    resto = resto%10
+                    break
+                case 'PC':
+                    j['PP']=resto
+                    break
+            }
+        }
+        actualizar()
+        aviso.innerHTML= 'Transaccion terminada'
+    }
 })
